e7d8531c934c6df9e081a48936fdc48685cdc1a3
Mateusz Mroziński <mateuszm1309@gmail.com>
2025-11-29 20:16:44 +0100

    vite plugin pwa, dodanie widoku mobilnego, placeholdery ikon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true,
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Sentiment Map",
+        short_name: "SentimentMap",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/app-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/app-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
